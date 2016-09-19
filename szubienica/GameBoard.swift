d7cea6377fd0b0d240e062f910626373ab52e3e2
miltenkot <miltenkot@gmail.com>
2016-09-19 11:10:52 +0200

    Dodanie zmiennej badchars

--- szubienica/GameBoard.swift
+++ szubienica/GameBoard.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class GameBoard: UIViewController {
 
+    @IBOutlet weak var attemptsLabel: UILabel!
     let wordlist = ["agrest","bigos","czara","obuwie"]
     override func viewDidLoad() {
         super.viewDidLoad()
         let target =
         "\(wordlist[Int(arc4random_uniform(UInt32(wordlist.count)))])"
     
-        _ = target.characters.count
+        let lenght = target.characters.count
+        var attemt = ""
+        for _ in 0..<lenght{
+            attemt += "-  "
+        }
+        attemptsLabel.text = attemt
+        var badchars = ""
+        
+        
+        
      
         
 
